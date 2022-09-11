eaff2bedf4ad43d0e8b9caef04fc7c2602c96649
Onur Özdemir <oozdemironur@gmail.com>
2022-09-11 22:12:58 +0300

    pages

--- dist/_nuxt/static/1662923061/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FmountainsWithFetchHook","\u002FmountainsWithAsyncDataHook","\u002Fmount-kilimanjaro","\u002Fvinson-massif","\u002Faconcagua","\u002Fdenali","\u002Fmount-kosciuszko","\u002Fmount-blanc","\u002Fmount-everest"]})
