e8e453eba0f9a85e4e4bb79e6b3426c8a1c0ef83
Onur Özdemir <oozdemironur@gmail.com>
2022-09-11 22:13:12 +0300

    Updates

--- _nuxt/static/1662923428/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FmountainsWithAsyncDataHook","\u002FmountainsWithFetchHook","\u002Fmount-everest","\u002Fmount-blanc","\u002Fvinson-massif","\u002Fmount-kilimanjaro","\u002Faconcagua","\u002Fmount-kosciuszko","\u002Fdenali"]})
--- _nuxt/static/1662923428/mount-kilimanjaro/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-exercise\u002F_nuxt\u002Fstatic\u002F1662923428",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmount-kilimanjaro",config:{_app:{basePath:"\u002Fnuxt-exercise\u002F",assetsPath:"\u002Fnuxt-exercise\u002F_nuxt\u002F",cdnURL:a}}}}(null));
