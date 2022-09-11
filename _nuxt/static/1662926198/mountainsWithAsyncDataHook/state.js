930c5f2ecea723ff21517c2d51ed1ea08e9955b8
Onur Özdemir <oozdemironur@gmail.com>
2022-09-11 22:56:43 +0300

    Updates

---
+++ _nuxt/static/1662926198/mountainsWithAsyncDataHook/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-exercise\u002F_nuxt\u002Fstatic\u002F1662926198",layout:"default",error:a,serverRendered:true,routePath:"\u002FmountainsWithAsyncDataHook",config:{_app:{basePath:"\u002Fnuxt-exercise\u002F",assetsPath:"\u002Fnuxt-exercise\u002F_nuxt\u002F",cdnURL:a}}}}(null));
